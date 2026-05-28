3f60a7aff83ae6f6b08172e7a27e07397be6dd20
L-Felipe-76 <luizfelipeto76@gmail.com>
2026-05-28 01:28:19 -0300

    Finalizado_Modificacao_Views

--- projetoBairroSeguro/resources/views/comingSoon.blade.php
+++ projetoBairroSeguro/resources/views/comingSoon.blade.php
@@ -2,6 +2,11 @@
 
 @section('titulo', 'BS - Ocorrências Recentes')
 
+@section('classe', 'mainSectionComingSoon')
+
 @section('conteudoPrincipal')
-    <h1>Em Breve</h1>
+<div id="comingSoonH1">
+    <h1 >Em Breve</h1>
+</div>    
+
 @endsection
